cf2b5294134e58e319e0c358b74f41db50c74289
SyuleymanMidyur <syuleyman.midyur@proton.me>
2024-05-18 23:54:01 -0500

    delete

--- programming_basic_python/first_step_in_coding_lab/pet_shop.py
+++
@@ -1,8 +0,0 @@
-nums_of_packeges_dog_food = int(input())
-nums_of_packeges_cat_food = int(input())
-
-price_for_packet_dog_food = 2.50 * nums_of_packeges_dog_food
-price_for_packet_cat_food = 4 * nums_of_packeges_cat_food
-final_price = price_for_packet_dog_food + price_for_packet_cat_food
-
-print(f"{final_price} lv.")
--- programming_basic_python/first_step_in_coding_lab/project_creation.py
+++
@@ -1,9 +0,0 @@
-architects_name = input()
-number_of_projects = int(input())
-needed_time_to_complate = number_of_projects *3
-
-
-
-
-
-print(f"The architect {architects_name} will need {needed_time_to_complate} hours to complete {number_of_projects} project/s.")
