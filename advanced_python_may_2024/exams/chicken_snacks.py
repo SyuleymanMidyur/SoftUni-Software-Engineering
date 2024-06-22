9847ed79a2c3cd24049948c2c17b0c5818a95d7d
SyuleymanMidyur <syuleyman.midyur@proton.me>
2024-06-22 13:39:27 +0300

    Homeworks_and_Labs

---
+++ advanced_python_may_2024/exams/chicken_snacks.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+money = [int(el) for el in input().split()]
+food_prices = deque([int(el) for el in input().split()])
+food_count = 0
+
+while money and food_prices:
+    if money[-1] == food_prices[0]:
+        food_count += 1
+        food_prices.popleft()
+        money.pop()
+    elif money[-1] > food_prices[0]:
+        food_count += 1
+        difference = money.pop() - food_prices.popleft()
+        if not money:
+            money = [difference]
+        else:
+            money[-1] += difference
+    else:
+        money.pop()
+        food_prices.popleft()
+
+if food_count >= 4:
+    print(f"Gluttony of the day! Henry ate {food_count} foods.")
+elif 1 < food_count < 4:
+    print(f"Henry ate: {food_count} foods.")
+elif food_count == 1:
+    print(f"Henry ate: {food_count} food.")
+else:
+    print("Henry remained hungry. He will try next weekend again.")
